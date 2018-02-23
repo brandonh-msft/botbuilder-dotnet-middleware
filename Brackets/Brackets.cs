989da137ba6313b0bac302213a67f816b5ab07fb
Brandon Hurlburt <hurlburb@microsoft.com>
2018-02-23 14:55:14 -0800

    Adding TestBot & 'Brackets' middleware

---
+++ Brackets/Brackets.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Middleware;
+using Microsoft.Bot.Schema;
+
+namespace BracketsMiddleware
+{
+    public class Brackets : IMiddleware, IReceiveActivity, ISendActivity
+    {
+        public async Task ReceiveActivity(IBotContext context, MiddlewareSet.NextDelegate next)
+        {
+            context.Reply("{");
+            await next();
+        }
+
+        public async Task SendActivity(IBotContext context, IList<IActivity> activities, MiddlewareSet.NextDelegate next)
+        {
+            await next();
+            context.Reply("}");
+        }
+    }
+}
---
+++ TestBot/Startup.cs
@@ -0,0 +1,39 @@
+﻿using BracketsMiddleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TestBot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new BotFrameworkAdapter(Configuration)
+                .Use(new Brackets()));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
